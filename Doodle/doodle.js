58b65f501fd047777416cf084f155727b8a0c978
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-08-08 00:06:18 -0500

    bar

--- Doodle/doodle.js
+++ Doodle/doodle.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
+    let isGmaeOVer = false
 
     function createDoodler(){
         grid.appendChild(doodler)
@@ -10,7 +11,36 @@ document.addEventListener('DOMContentLoaded', () =>{
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
+
+    class Platform{
+        constructor(newPlatBottom){
+            this.bottom = newPlatBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom+ 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms(){
+        for(let i=0; i<platCount; i++){
+            let platGap = 600/ platCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform()
+        }
+    }
+
+    function start(){
+        if (isGmaeOVer == false){
+            createDoodler()
+            createPlatforms()
+        }
+    }
     createDoodler()
 
-    
+    //attach to buttin
 })
