bc8345936226dca869a6929b8cb05516c88610a9
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-09-17 22:48:36 -0500

    jump

--- Bird/app.js
+++ Bird/app.js
@@ -4,11 +4,31 @@ document.addEventListener('DOMContentLoaded' , () => {
     const ground = document.querySelector('.ground-moving')
 
 
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 3
+    let isGameOver = false
+    let gap = 430
+
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
 
+    let gameTimerId = setInterval(startGame, 20)
 
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+
+    function jump() {
+        if (birdBottom < 500) birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+    }
+    document.addEventListener('keyup', control)
+    
 })
